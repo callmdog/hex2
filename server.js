629ff0804062378e35b14552ce459be74bb52730
callmdog <157610989+callmdog@users.noreply.github.com>
2024-02-13 15:38:32 +0100

    Actualizar server.js

--- server.js
+++ server.js
@@ -457,10 +457,10 @@ socket.on('updatePlayersRequest', () => {
     ///SISTEMA PUNTOS////////////////////
     socket.on('greenCircleEaten', () => {
     const playerId = socket.id;
-    players[playerId].puntos += 0.5; // Sumar 10 puntos por cada círculo verde comido
-// io.emit('updatePlayers', players); 
+    players[playerId].puntos += 1; // Sumar 10 puntos por cada círculo verde comido
+io.emit('updatePlayers', players); 
         // Actualizar la información de los jugadores para todos
-             //   io.emit('updatePlayers2', players);
+               io.emit('updatePlayers2', players);
 
     console.log(`+ Puntos: ${players[playerId].puntos}, ${players[playerId].nombre} `);
 
