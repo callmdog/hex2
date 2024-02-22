3ff1e842f31b6ae3df5f25c0c1a11066f21c9864
callmdog <157610989+callmdog@users.noreply.github.com>
2024-02-22 22:31:12 +0100

    Update server.js

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ function generateAllHexagonVertices() {
 const allHexagonVertices = generateAllHexagonVertices();
 console.log(allHexagonVertices); // Opcional: Imprime los vértices en la consola para visualizarlos
 
-
+console.log('LENGTH: !!!!!!!!!!!!!'allHexagonVertices.length);
 
 
 //////////////////////////////////////////////////////////////////////////////////////
